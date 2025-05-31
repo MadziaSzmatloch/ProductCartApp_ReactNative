c794b28beca5b75934c75cea7d9532f8a8891e5a
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2025-05-31 17:35:37 +0200

    skeleton made

---
+++ ProductCartApp/Gateway/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> GetAsync()
+        {
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("http://product:8080/");
+
+            response.EnsureSuccessStatusCode();
+
+            using var httpClient2 = new HttpClient();
+
+            var response1 = await httpClient2.GetAsync("http://cart:8080/");
+
+            response.EnsureSuccessStatusCode();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
